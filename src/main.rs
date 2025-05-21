edd4e11d9bc768b9061a63d88e33d07a14ccccdd
zerothy <zxuyagaming@gmail.com>
2025-05-22 03:16:57 +0700

    Experiment 1.1: Original timer from the book

--- src/main.rs
+++ src/main.rs
@@ -99,10 +99,10 @@ fn main() {
 
     // Spawn a task to print before and after waiting on a timer.
     spawner.spawn(async {
-        println!("howdy!");
+        println!("Joe's Komputer: howdy!");
         // Wait for our timer future to complete after two seconds.
         TimerFuture::new(Duration::new(2, 0)).await;
-        println!("done!");
+        println!("Joe's Komputer: done!");
     });
 
     // Drop the spawner so that our executor knows it is finished and won't
